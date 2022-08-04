58e2c2ebe33ce92f2292d0f73f9953d279a1a147
ajmairkashif <107035571+ajmairkashif@users.noreply.github.com>
2022-08-04 11:12:15 +0500

    Update test-webcam.py

--- test-webcam.py
+++ test-webcam.py
@@ -10,7 +10,7 @@ import socket
 hostname = socket.gethostname()
 # getting the IP address using socket.gethostbyname() method
 ip_address = socket.gethostbyname(hostname)
-# printing the hostname and ip_address
+# capturing the video from ip stream 
 cap = cv2.VideoCapture(f'http://{ip_address}:56000/mjpeg')
 # cap.open("")
 
