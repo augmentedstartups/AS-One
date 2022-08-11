ca5b39f2ba1c2a60ffcee8f062fec7fc5ba2da2f
IamUmairImran <umair@axcelerate.ai>
2022-08-11 10:16:00 +0500

    Add command to install nvidia-docker2

--- docker-installation.sh
+++ docker-installation.sh
@@ -3,6 +3,7 @@
 echo "Removing previous verions of dockers"
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
+
 echo "Updating apt-package index..."
 sudo apt-get update -y
 
@@ -16,7 +17,7 @@ sudo apt-get install \
 
 echo "Adding Docker GPG Key..."
 
-sudo mkdir -p /etc/apt/keyrings -y
+sudo mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
@@ -27,10 +28,13 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installing Docker Engine..."
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 sudo docker --version
+
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
 
 sudo xhost +local:docker
 
