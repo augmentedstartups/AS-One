f1481ea1ff78bc3e32e66d7ec71901e98615d1cf
IamUmairImran <umair.imran@axcelerate.ai>
2022-09-02 15:54:38 +0500

    Add function to draw detections

--- asone-linux/code/asone/utils.py
+++ asone-linux/code/asone/utils.py
@@ -2,16 +2,48 @@ from asone.detectors import YOLOv5Detector
 from asone.detectors import YOLOv7Detector
 from asone.trackers import ByteTrack
 from asone.trackers import NorFair
+from asone.trackers import DeepSort
+import cv2
+from numpy import random
+import numpy as np
+from collections import deque
+import math
 
-detectors={
-    'yolov5s' : YOLOv5Detector,
-    'yolov7' : YOLOv7Detector
+
+detectors = {
+    'yolov5s': YOLOv5Detector,
+    'yolov7': YOLOv7Detector
 }
 
-trackers={
-    'byte_track' : ByteTrack,
-    'norfair': NorFair
+trackers = {
+    'byte_track': ByteTrack,
+    'norfair': NorFair,
+    'deepsort': DeepSort
 }
+
+
+def load_classes(path):
+    # Loads *.names file at 'path'
+    with open(path, 'r') as f:
+        names = f.read().split('\n')
+    # filter removes empty strings (such as last line)
+    return list(filter(None, names))
+
+
+global names
+names = load_classes('asone/detectors/data/coco.names')
+
+
+colors = [[random.randint(0, 255) for _ in range(3)]
+          for _ in range(len(names))]
+palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+data_deque = {}
+speed_four_line_queue = {}
+object_counter = {}
+
+# line1 = [(250,450), (1000, 450)]
+
+line2 = [(200, 500), (1050, 500)]
 
 
 def get_detector(detector, use_cuda=True, use_onnx=False):
@@ -22,6 +54,7 @@ def get_detector(detector, use_cuda=True, use_onnx=False):
     else:
         return None
 
+
 def get_tracker(tracker, detector, use_cuda=True, use_onnx=False):
     tracker = trackers.get(tracker, None)
 
@@ -30,3 +63,210 @@ def get_tracker(tracker, detector, use_cuda=True, use_onnx=False):
     else:
         return None
 
+
+def draw_border(img, pt1, pt2, color, thickness, r, d):
+    x1, y1 = pt1
+    x2, y2 = pt2
+    # Top left
+    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
+    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+
+    # Top right
+    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
+    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+    # Bottom left
+    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
+    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+    # Bottom right
+    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
+    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+
+    cv2.rectangle(img, (x1 + r, y1), (x2 - r, y2), color, -1, cv2.LINE_AA)
+    cv2.rectangle(img, (x1, y1 + r), (x2, y2 - r - d), color, -1, cv2.LINE_AA)
+
+    cv2.circle(img, (x1 + r, y1+r), 2, color, 12)
+    cv2.circle(img, (x2 - r, y1+r), 2, color, 12)
+    cv2.circle(img, (x1 + r, y2-r), 2, color, 12)
+    cv2.circle(img, (x2 - r, y2-r), 2, color, 12)
+
+    return img
+
+
+def xyxy_to_xywh(xyxy):
+    """" Calculates the relative bounding box from absolute pixel values. """
+    bbox_left = min([xyxy[0], xyxy[2]])
+    bbox_top = min([xyxy[1], xyxy[3]])
+    bbox_w = abs(xyxy[0] - xyxy[2])
+    bbox_h = abs(xyxy[1] - xyxy[3])
+    x_c = (bbox_left + bbox_w / 2)
+    y_c = (bbox_top + bbox_h / 2)
+    w = bbox_w
+    h = bbox_h
+    return [x_c, y_c, w, h]
+
+def tlwh_to_xyxy(tlwh):
+    """" Convert tlwh to xyxy """
+    x1 = tlwh[0]
+    y1 = tlwh[1]
+    x2 = tlwh[2] + x1
+    y2 = tlwh[3] + y1
+    return [x1, y1, x2, y2]
+
+
+
+def xyxy_to_tlwh(bbox_xyxy):
+    tlwh_bboxs = []
+    for i, box in enumerate(bbox_xyxy):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        top = x1
+        left = y1
+        w = int(x2 - x1)
+        h = int(y2 - y1)
+        tlwh_obj = [top, left, w, h]
+        tlwh_bboxs.append(tlwh_obj)
+    return tlwh_bboxs
+
+
+def compute_color_for_labels(label):
+    """
+    Simple function that adds fixed color depending on the class
+    """
+    if label == 0:  # person  #BGR
+        color = (85, 45, 255)
+    elif label == 2:  # Car
+        color = (222, 82, 175)
+    elif label == 3:  # Motobike
+        color = (0, 204, 255)
+    elif label == 5:  # Bus
+        color = (0, 149, 255)
+    else:
+        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    return tuple(color)
+
+
+def UI_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(
+        0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    # cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        # c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+
+        img = draw_border(img, (c1[0], c1[1] - t_size[1] - 3),
+                          (c1[0] + t_size[0], c1[1]+3), color, 1, 8, 2)
+
+        # cv2.line(img, c1, c2, color, 30)
+        # cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3,
+                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+def estimateSpeed(location1, location2):
+
+    d_pixels = math.sqrt(math.pow(
+        location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
+    ppm = 8  # Pixels per Meter
+    d_meters = d_pixels / ppm
+    time_constant = 15 * 3.6
+    speed = d_meters * time_constant
+    return speed
+
+# Return true if line segments AB and CD intersect
+
+
+def intersect(A, B, C, D):
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+
+
+def ccw(A, B, C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+
+def draw_boxes(img, bbox_xyxy, object_id, identities=None, offset=(0, 0)):
+    # cv2.line(img, line2[0], line2[1], (0,200,0), 3)
+
+    height, width, _ = img.shape
+    # remove tracked point from buffer if object is lost
+    for key in list(data_deque):
+        if key not in identities:
+            data_deque.pop(key)
+
+    for i, box in enumerate(bbox_xyxy):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        x1 += offset[0]
+        x2 += offset[0]
+        y1 += offset[1]
+        y2 += offset[1]
+
+        # box_area = (x2-x1) * (y2-y1)
+        box_height = (y2-y1)
+
+        # code to find center of bottom edge
+        center = (int((x2+x1) / 2), int((y2+y2)/2))
+
+        # get ID of object
+        id = int(identities[i]) if identities is not None else 0
+
+        # create new buffer for new object
+        if id not in data_deque:
+            data_deque[id] = deque(maxlen=64)
+            speed_four_line_queue[id] = []
+
+        color = compute_color_for_labels(object_id[i])
+        obj_name = names[object_id[i]]
+        label = '%s' % (obj_name)
+
+        # add center to buffer
+        data_deque[id].appendleft(center)
+
+        # print("id ", id)
+        # print("data_deque[id] ", data_deque[id])
+
+        if len(data_deque[id]) >= 2:
+            # print("data_deque[id][i-1]", data_deque[id][1], data_deque[id][0])
+
+            # or intersect(data_deque[id][0], data_deque[id][1], line1[0], line1[1]) or intersect(data_deque[id][0], data_deque[id][1], line3[0], line3[1]) or intersect(data_deque[id][0], data_deque[id][1], line4[0], line4[1]) :
+            if intersect(data_deque[id][0], data_deque[id][1], line2[0], line2[1]):
+
+                # cv2.line(img, line2[0], line2[1], (0,100,0), 3)
+
+                obj_speed = estimateSpeed(data_deque[id][1], data_deque[id][0])
+
+                speed_four_line_queue[id].append(obj_speed)
+
+                if obj_name not in object_counter:
+                    object_counter[obj_name] = 1
+                else:
+                    object_counter[obj_name] += 1
+
+        try:
+            label = label + " " + \
+                str(sum(speed_four_line_queue[id]) //
+                    len(speed_four_line_queue[id]))
+        except:
+            pass
+
+        UI_box(box, img, label=label, color=color, line_thickness=2)
+
+        # draw trail
+        for i in range(1, len(data_deque[id])):
+            # check if on buffer value is none
+            if data_deque[id][i - 1] is None or data_deque[id][i] is None:
+                continue
+
+            # generate dynamic thickness of trails
+            thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
+
+            # draw trails
+            cv2.line(img, data_deque[id][i - 1],
+                     data_deque[id][i], color, thickness)
+
+    return img
