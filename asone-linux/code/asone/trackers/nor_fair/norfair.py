b83122429d389e5ce2af9e302c311e93e439a72d
IamUmairImran <umair.imran@axcelerate.ai>
2022-09-02 17:08:32 +0500

    Add input shape for detector

--- asone-linux/code/asone/trackers/nor_fair/norfair.py
+++ asone-linux/code/asone/trackers/nor_fair/norfair.py
@@ -5,19 +5,20 @@ import numpy as np
 
 class NorFair:
     def __init__(self, detector, max_distance_between_points=30) -> None:
-        
+
         self.tracker = Tracker(
             distance_function=self._euclidean_distance,
             distance_threshold=max_distance_between_points,
         )
         self.detector = detector
-
+        self.input_shape = tuple(detector.model.get_inputs()[0].shape[2:])
 
     def _euclidean_distance(self, detection, tracked_object):
         return np.linalg.norm(detection.points - tracked_object.estimate)
 
     def detect_and_track(self, image):
-        _dets_xyxy, image_info = self.detector.detect(image)
+        _dets_xyxy, image_info = self.detector.detect(
+            image, input_shape=self.input_shape)
 
         class_ids = []
         ids = []
@@ -27,13 +28,15 @@ class NorFair:
         if isinstance(_dets_xyxy, np.ndarray) and len(_dets_xyxy) > 0:
 
             dets_xyxy = [
-                Detection(np.array([(box[2] + box[0])/2, (box[3] + box[1])/2]), data=box)
+                Detection(
+                    np.array([(box[2] + box[0])/2, (box[3] + box[1])/2]), data=box)
                 for box in _dets_xyxy
                 # if box[-1] == 2
             ]
 
-            bboxes_xyxy, ids,  scores, class_ids = self._tracker_update(dets_xyxy, image_info)
-    
+            bboxes_xyxy, ids,  scores, class_ids = self._tracker_update(
+                dets_xyxy, image_info)
+
         return bboxes_xyxy, ids,  scores, class_ids
 
     def _tracker_update(self, dets_xyxy: list, image_info: dict):
