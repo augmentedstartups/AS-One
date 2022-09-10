d616209bda85b7a12168e88187a1ea26e9289f67
IamUmairImran <umair.imran@axcelerate.ai>
2022-09-10 23:24:55 +0500

    Change tracker in param

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('video_path', help='Path to input video')
-    parser.add_argument('--tracker', type=int, default=0, help='Path to input video')
+    parser.add_argument('--tracker', type=int, default=0, help='tracker [0, 1, 2] for Bytetrack, deepsort, norfair')
     parser.add_argument('--detector',default='yolov5s', help='Path to input video')
     parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda', help='run on cpu')
     args = parser.parse_args()
