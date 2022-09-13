ff4898c624a2a2564a163f53ab143434c2f2055b
IamUmairImran <umair.imran@axcelerate.ai>
2022-09-13 11:58:39 +0500

    Add cfgs and other data in package

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import setup
 from pkg_resources import parse_requirements
 import pathlib
 
@@ -10,17 +11,14 @@ DOWNLOAD_URL = URL
 
 VERSION = '0.1-dev'
 
-from setuptools import setup
-
 
 requirements_txt = pathlib.Path('requirements.txt').open()
-
 
 
 def setup_package():
     setup(
         name=DISTNAME,
-        version=VERSION,    
+        version=VERSION,
         description=DESCRIPTION,
         url=DOWNLOAD_URL,
         author=MAINTAINER,
@@ -28,49 +26,53 @@ def setup_package():
         license='BSD 2-clause',
         # package_dir={"":""},
         packages=[DISTNAME,
-                    f'{DISTNAME}.detectors',
-                    f'{DISTNAME}.detectors.utils',
-                    f'{DISTNAME}.detectors.yolov5',
-                    f'{DISTNAME}.detectors.yolov5.models',
-                    f'{DISTNAME}.detectors.yolov5.utils',
-                    f'{DISTNAME}.detectors.yolov6',
-                    f'{DISTNAME}.detectors.yolov6.layers',
-                    f'{DISTNAME}.detectors.yolov6.models',
-                    f'{DISTNAME}.detectors.yolov6.utils',
-                    f'{DISTNAME}.detectors.yolov7',
-                    f'{DISTNAME}.detectors.yolov7.models',
-                    f'{DISTNAME}.detectors.yolov7.utils',
-                    f'{DISTNAME}.detectors.yolor',
-                    f'{DISTNAME}.detectors.yolor.models',
-                    f'{DISTNAME}.detectors.yolor.utils',
-                    f'{DISTNAME}.detectors.yolox',
-                    f'{DISTNAME}.detectors.yolox.yolox',
-                    f'{DISTNAME}.detectors.yolox.yolox.exp',
-                    f'{DISTNAME}.detectors.yolox.yolox.models',
-                    f'{DISTNAME}.detectors.yolox.yolox.utils',
-                    f'{DISTNAME}.trackers',
-                    f'{DISTNAME}.trackers.byte_track',
-                    f'{DISTNAME}.trackers.byte_track.tracker',
-                    f'{DISTNAME}.trackers.deep_sort',
-                    f'{DISTNAME}.trackers.deep_sort.tracker',
-                    f'{DISTNAME}.trackers.deep_sort.tracker.deep',
-                    f'{DISTNAME}.trackers.deep_sort.tracker.sort',
-                    f'{DISTNAME}.trackers.nor_fair',
-                    f'{DISTNAME}.utils',
-                    ],
+                  f'{DISTNAME}.detectors',
+                  f'{DISTNAME}.detectors.utils',
+                  f'{DISTNAME}.detectors.yolov5',
+                  f'{DISTNAME}.detectors.yolov5.models',
+                  f'{DISTNAME}.detectors.yolov5.utils',
+                  f'{DISTNAME}.detectors.yolov6',
+                  f'{DISTNAME}.detectors.yolov6.layers',
+                  f'{DISTNAME}.detectors.yolov6.models',
+                  f'{DISTNAME}.detectors.yolov6.utils',
+                  f'{DISTNAME}.detectors.yolov7',
+                  f'{DISTNAME}.detectors.yolov7.models',
+                  f'{DISTNAME}.detectors.yolov7.utils',
+                  f'{DISTNAME}.detectors.yolor',
+                  f'{DISTNAME}.detectors.yolor.models',
+                  f'{DISTNAME}.detectors.yolor.utils',
+                  f'{DISTNAME}.detectors.yolox',
+                  f'{DISTNAME}.detectors.yolox.yolox',
+                  f'{DISTNAME}.detectors.yolox.yolox.exp',
+                  f'{DISTNAME}.detectors.yolox.yolox.models',
+                  f'{DISTNAME}.detectors.yolox.yolox.utils',
+                  f'{DISTNAME}.trackers',
+                  f'{DISTNAME}.trackers.byte_track',
+                  f'{DISTNAME}.trackers.byte_track.tracker',
+                  f'{DISTNAME}.trackers.deep_sort',
+                  f'{DISTNAME}.trackers.deep_sort.tracker',
+                  f'{DISTNAME}.trackers.deep_sort.tracker.deep',
+                  f'{DISTNAME}.trackers.deep_sort.tracker.sort',
+                  f'{DISTNAME}.trackers.nor_fair',
+                  f'{DISTNAME}.utils',
+                  ],
 
-        dependency_links = [
-        "https://download.pytorch.org/whl/cu113/",
-        'https://pypi.python.org/simple/'],
-        install_requires=[str(requirement) for requirement in parse_requirements(requirements_txt)],
+        dependency_links=[
+            "https://download.pytorch.org/whl/cu113/",
+            'https://pypi.python.org/simple/'],
+        install_requires=[str(requirement)
+                          for requirement in parse_requirements(requirements_txt)],
         package_data={
-                "": ["*.cfg", "*.ymal", "*.yml", "*.names"],
-            },
+            "": ["detectors/yolor/cfg/*.cfg", "detectors/data/*.yaml",
+                         "detectors/data/*.yml", "detectors/data/*.names"],
+        },
+
+        include_package_data=True,
         classifiers=[
             'Development Status :: 1 - Planning',
             'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: BSD License',  
-            'Operating System :: POSIX :: Linux',        
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX :: Linux',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
@@ -80,6 +82,7 @@ def setup_package():
             'Programming Language :: Python :: 3.9',
         ],
     )
+
 
 if __name__ == "__main__":
     setup_package()
