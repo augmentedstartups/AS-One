22d5f594fb25d4567769d567022387a975a418ef
ajmairkashif <ajmair.kashif@axcelerate.ai>
2022-09-26 21:33:54 +0500

    Add exps in yolox

--- asone/detectors/yolov5/__init__.py
+++ asone/detectors/yolov5/__init__.py
@@ -1,7 +1,2 @@
-# import os
-# import sys
-# # sys.path.append(os.path.join(os.path.dirname(__file__), 'yolov5'))
-# sys.path.insert(0, 'asone/detectors/yolov5/yolov5')
-
 from .yolov5_detector import YOLOv5Detector
 __all__ = ['YOLOv5Detector']
