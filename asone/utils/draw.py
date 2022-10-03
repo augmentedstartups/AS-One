73053ce2221a0992f2f24f10fe728fea01c59916
IamUmairImran <umair.imran@axcelerate.ai>
2022-10-03 18:27:26 +0500

    Modify draw_bbox to draw box

--- asone/utils/draw.py
+++ asone/utils/draw.py
@@ -1,30 +1,23 @@
 import cv2
 from numpy import random
 import numpy as np
-from collections import deque
 from asone.utils import compute_color_for_labels
-from asone.utils import intersect, estimateSpeed
 from asone.utils import get_names
 
 names = get_names()
 
-data_deque = {}
-speed_four_line_queue = {}
-object_counter = {}
-line2 = [(200, 500), (1050, 500)]
 
-
-
-def draw_ui_box(x, img, color=None, label=None, line_thickness=None):
+def draw_ui_box(x, img, id, color=None, label=None, line_thickness=None):
     # Plots one bounding box on image img
     tl = line_thickness or round(
         0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    # cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    
     if label:
         tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        t_size = cv2.getTextSize(str(id), 0, fontScale=tl / 3, thickness=tf)[0]
         # c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
 
         img = draw_border(img, (c1[0], c1[1] - t_size[1] - 3),
@@ -32,7 +25,7 @@ def draw_ui_box(x, img, color=None, label=None, line_thickness=None):
 
         # cv2.line(img, c1, c2, color, 30)
         # cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3,
+        cv2.putText(img, str(id), (c1[0], c1[1] - 2), 0, tl / 3,
                     [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
@@ -73,10 +66,7 @@ def draw_boxes(img, bbox_xyxy, object_id, identities=None, offset=(0, 0)):
 
     height, width, _ = img.shape
     # remove tracked point from buffer if object is lost
-    for key in list(data_deque):
-        if key not in identities:
-            data_deque.pop(key)
-
+  
     for i, box in enumerate(bbox_xyxy):
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -93,58 +83,10 @@ def draw_boxes(img, bbox_xyxy, object_id, identities=None, offset=(0, 0)):
         # get ID of object
         id = int(identities[i]) if identities is not None else 0
 
-        # create new buffer for new object
-        if id not in data_deque:
-            data_deque[id] = deque(maxlen=64)
-            speed_four_line_queue[id] = []
-
+   
         color = compute_color_for_labels(object_id[i])
         obj_name = names[object_id[i]]
         label = '%s' % (obj_name)
 
-        # add center to buffer
-        data_deque[id].appendleft(center)
-
-        # print("id ", id)
-        # print("data_deque[id] ", data_deque[id])
-
-        if len(data_deque[id]) >= 2:
-            # print("data_deque[id][i-1]", data_deque[id][1], data_deque[id][0])
-
-            # or intersect(data_deque[id][0], data_deque[id][1], line1[0], line1[1]) or intersect(data_deque[id][0], data_deque[id][1], line3[0], line3[1]) or intersect(data_deque[id][0], data_deque[id][1], line4[0], line4[1]) :
-            if intersect(data_deque[id][0], data_deque[id][1], line2[0], line2[1]):
-
-                # cv2.line(img, line2[0], line2[1], (0,100,0), 3)
-
-                obj_speed = estimateSpeed(data_deque[id][1], data_deque[id][0])
-
-                speed_four_line_queue[id].append(obj_speed)
-
-                if obj_name not in object_counter:
-                    object_counter[obj_name] = 1
-                else:
-                    object_counter[obj_name] += 1
-
-        try:
-            label = label + " " + \
-                str(sum(speed_four_line_queue[id]) //
-                    len(speed_four_line_queue[id]))
-        except:
-            pass
-
-        draw_ui_box(box, img, label=label, color=color, line_thickness=2)
-
-        # draw trail
-        for i in range(1, len(data_deque[id])):
-            # check if on buffer value is none
-            if data_deque[id][i - 1] is None or data_deque[id][i] is None:
-                continue
-
-            # generate dynamic thickness of trails
-            thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
-
-            # draw trails
-            cv2.line(img, data_deque[id][i - 1],
-                     data_deque[id][i], color, thickness)
-
+        draw_ui_box(box, img, id, label=label, color=color, line_thickness=2)
     return img
