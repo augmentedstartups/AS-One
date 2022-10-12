0993c2568a7a4f6be96e0b9a8426bb6e30efa342
IamUmairImran <umair.imran@axcelerate.ai>
2022-10-12 17:46:18 +0500

    resolve path issue

--- asone/detectors/yolox/yolox/exp/build.py
+++ asone/detectors/yolox/yolox/exp/build.py
@@ -9,7 +9,10 @@ import sys
 
 def get_exp_by_file(exp_file):
     try:
-        sys.path.append(os.path.dirname(exp_file))
+        _dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        _dir = os.path.join(_dir, 'exps')
+        sys.path.append(_dir)
+
         current_exp = importlib.import_module(os.path.basename(exp_file).split(".")[0])
         exp = current_exp.Exp()
     except Exception:
