617c8c16a25de41ad8eff634f0d6458240a8f00d
IamUmairImran <umair.imran@axcelerate.ai>
2022-10-25 13:50:24 +0500

    Add arguments for display and save_results

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import argparse
 
 def main(args):
     dt_obj = ASOne(tracker=asone.BYTETRACK, detector=asone.YOLOX_DARKNET_PYTORCH, use_cuda=args.use_cuda)
-    dt_obj.track_video(args.video_path)
+    dt_obj.track_video(args.video_path, save_result=args.save_result, display=args.display)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('video_path', help='Path to input video')
-    parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda', help='run on cpu')
+    parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda', help='run on cpu if not provided the program will run on gpu.')
+    parser.add_argument('--save', default=False, action='store_true', dest='save_result', help='if provided the results will save in output directory')
+    parser.add_argument('--display', default=False, action='store_true', dest='display', help='if provided the results will be displayed on screen')
+    parser.add_argument('--output_dir', default='results',  help='Path to output directory')
+    
     args = parser.parse_args()
 
     main(args)
