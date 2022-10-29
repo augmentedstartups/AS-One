3587cb4600765552f40219c4ecb4a6776c5c4a09
IamUmairImran <umair.imran@axcelerate.ai>
2022-10-29 18:38:37 +0500

    Add arguments for testing

--- demo_detector.py
+++ demo_detector.py
@@ -2,14 +2,24 @@ import asone
 from asone import utils
 from asone.detectors import Detector
 import cv2
+import argparse
 
-img = cv2.imread('sample_imgs/test2.jpg')
-detector = Detector(asone.YOLOV7_E6_ONNX, use_cuda=True).get_detector()
-dets, img_info = detector.detect(img)
+def main(args):
+    img = cv2.imread('sample_imgs/test2.jpg')
+    detector = Detector(asone.YOLOV7_E6_ONNX, use_cuda=args.use_cuda).get_detector()
+    dets, img_info = detector.detect(img)
 
-bbox_xyxy = dets[:, :4]
-scores = dets[:, 4]
-class_ids = dets[:, 5]
+    bbox_xyxy = dets[:, :4]
+    scores = dets[:, 4]
+    class_ids = dets[:, 5]
 
-img = utils.draw_boxes(img, bbox_xyxy, class_ids=class_ids)
-cv2.imwrite('result.png', img)
+    img = utils.draw_boxes(img, bbox_xyxy, class_ids=class_ids)
+    cv2.imwrite('result.png', img)
+
+if __name__=='__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda', help='If provided the model will run on cpu otherwise it will run on gpu')
+
+    args = parser.parse_args()
+    main(args)
