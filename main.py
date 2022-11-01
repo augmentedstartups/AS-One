1a14c8ec756782e048f06921d6dc9aadaee8c66c
nash-m4 <naeem@axcelerate.ai>
2022-11-01 12:12:04 +0500

    Change arg paths

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from asone import ASOne
 import argparse
 
 def main(args):
-    dt_obj = ASOne(tracker=asone.BYTETRACK, detector=asone.YOLOX_DARKNET_PYTORCH, use_cuda=args.use_cuda)
-    dt_obj.track_video(args.video_path, save_result=args.save_result, display=args.display)
+    dt_obj = ASOne(
+        tracker=asone.BYTETRACK,
+        detector=asone.YOLOX_DARKNET_PYTORCH,
+        use_cuda=args.use_cuda
+        )
+    dt_obj.track_video(
+        args.video_path,
+        output_dir=args.output_dir,
+        save_result=args.save_result,
+        display=args.display
+        )
 
 
 if __name__ == '__main__':
@@ -18,7 +27,7 @@ if __name__ == '__main__':
                         dest='save_result', help='if provided the results will not save.')
     parser.add_argument('--no_display', default=True, action='store_false',
                         dest='display', help='if provided the results will not be displayed on screen')
-    parser.add_argument('--output_dir', default='results',  help='Path to output directory')
+    parser.add_argument('--output_dir', default='data/results',  help='Path to output directory')
     
     args = parser.parse_args()
 
