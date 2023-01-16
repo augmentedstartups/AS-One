37de4fbaee82f479c5ca8d53a2450c7083056ef1
IamUmairImran <umair.imran@axcelerate.ai>
2023-01-16 15:46:30 +0500

    [Detector] Use Yolov8 default preprocessing

--- asone/detectors/yolov8/utils/yolov8_utils.py
+++ asone/detectors/yolov8/utils/yolov8_utils.py
@@ -2,18 +2,14 @@ import cv2
 import numpy as np
 from ultralytics.yolo.utils import ops
 import torch
+from ultralytics.yolo.data.augment import LetterBox
 
-def prepare_input(image, input_shape):
-    input_height, input_width = input_shape
-    input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Resize input image
-    input_img = cv2.resize(input_img, (input_width, input_height))
-    # Scale input pixel values to 0 to 1
-    input_img = input_img / 255.0
-    input_img = input_img.transpose(2, 0, 1)
-    input_tensor = input_img[np.newaxis, :, :, :].astype(np.float32)
-
+def prepare_input(image, input_shape, stride, pt):
+    input_tensor = LetterBox(input_shape, auto=pt, stride=stride)(image=image)
+    input_tensor = input_tensor.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    input_tensor = np.ascontiguousarray(input_tensor).astype(np.float32)  # contiguous
+    input_tensor /= 255.0  # 0 - 255 to 0.0 - 1.0
+    input_tensor = input_tensor[None].astype(np.float32)
     return input_tensor
 
 
@@ -24,32 +20,25 @@ def process_output(detections,
                    iou_threshold,
                    classes=None,
                    agnostic=False,
-                   multi_label=False,
-                   labels=(),
                    max_det=300,
-                   nm=0,  # number of masks
                    ):
-    if not isinstance(detections, torch.Tensor):    
-        detections = torch.from_numpy(detections)
     detections = ops.non_max_suppression(detections,
                                           conf_thres=conf_threshold,
                                           iou_thres=iou_threshold,
                                           classes=classes,
                                           agnostic=agnostic,
-                                          multi_label=multi_label,
-                                          labels=labels,
                                           max_det=max_det,
-                                          nm=nm,  # number of masks
-                                        )
+                                          )
 
     for i in range(len(detections)):
 
         # convert tensor to numpy array
-        detections[i] = detections[i].cpu().numpy()
+        # detections[i] = detections[i].cpu().numpy()
         
         # Extract boxes from predictions
-        detections[i][:, :4] = rescale_boxes(
-            detections[i][:, :4], ori_shape, input_shape)
+        detections[i][:, :4] = ops.scale_boxes(input_shape, detections[i][:, :4], ori_shape).round()
+        # detections[i][:, :4] = rescale_boxes(
+        #     detections[i][:, :4], ori_shape, input_shape)
     
     return detections
 
--- asone/detectors/yolov8/yolov8_detector.py
+++ asone/detectors/yolov8/yolov8_detector.py
@@ -7,6 +7,7 @@ from .utils.yolov8_utils import prepare_input, process_output
 import numpy as np
 import warnings
 from ultralytics.nn.autobackend import AutoBackend
+from ultralytics.nn.tasks import DetectionModel, attempt_load_one_weight
 
 class YOLOv8Detector:
     def __init__(self,
@@ -39,11 +40,10 @@ class YOLOv8Detector:
                 providers = ['CPUExecutionProvider']
 
             model = onnxruntime.InferenceSession(weights, providers=providers)
-
         #Load Pytorch
         else:
-             
-            model = AutoBackend(weights=weights).to(self.device)
+            model, ckpt = attempt_load_one_weight(weights)
+            model = AutoBackend(model, fp16=False, dnn=False).to(self.device)
             model.half() if self.fp16 else model.float()
         return model
 
@@ -58,14 +58,15 @@ class YOLOv8Detector:
 
         # Preprocess input image and also copying original image for later use
         original_image = image.copy()
-        processed_image = prepare_input(image, input_shape)
-
+        processed_image = prepare_input(image, input_shape, 32, False if self.use_onnx else True)
+    
         # Perform Inference on the Image
         if self.use_onnx:
-        # Run ONNX model 
+            # Run ONNX model 
             input_name = self.model.get_inputs()[0].name
             prediction = self.model.run([self.model.get_outputs()[0].name], {
                                  input_name: processed_image})[0]
+            prediction = torch.from_numpy(prediction)
         # Run Pytorch model
         else:
             processed_image = torch.from_numpy(processed_image).to(self.device)
@@ -73,13 +74,13 @@ class YOLOv8Detector:
             processed_image = processed_image.half() if self.fp16 else processed_image.float() 
 
             with torch.no_grad():
-                prediction = self.model(processed_image, augment=False)[0]
+                prediction = self.model(processed_image, augment=False)
                 
         detection = []
         # Postprocess prediction
         detection = process_output(prediction,
                                     original_image.shape[:2],
-                                    input_shape,
+                                    processed_image.shape[2:],
                                     conf_thres,
                                     iou_thres)[0]
 
