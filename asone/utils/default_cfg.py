c90381fcb74ac509a6c0362ebe26afd2a0036df6
IamUmairImran <umair.imran@axcelerate.ai>
2023-01-18 15:04:08 +0500

    [ASONE] Add default config file

---
+++ asone/utils/default_cfg.py
@@ -0,0 +1,16 @@
+config = {
+    "output_dir": "results",
+    "filename": None,
+    "fps": None,
+    "save_result": True,
+    "display": True,
+    "draw_trails": False,
+    "filter_classes": None,
+    "class_names": None,
+    "input_shape" : (640, 640),
+    "conf_thres": 0.25,
+    "iou_thres" : 0.45,
+    "max_det" : 1000,
+    "agnostic_nms" : False,
+    "with_p6" : False
+}
