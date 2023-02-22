535aaa1afa0f2f12217c017519b7473dbfe9ce83
IamMeharRamzan <muhammad.ramzan@axcelerate.ai>
2023-02-22 16:28:18 +0500

    Add new class for text

--- asone/utils/classes.py
+++ asone/utils/classes.py
@@ -12,7 +12,7 @@ names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', '
         'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-        'hair drier', 'toothbrush']
+        'hair drier', 'toothbrush', "text"]
 
 def get_names():
     return names
