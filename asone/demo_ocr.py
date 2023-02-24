45f7625cde6f4a6e25e95d70cfe99701c89a6d2d
umair-imran <umair.imran@axcelerate.ai>
2023-02-24 19:59:27 +0500

    Add code to test ocr

--- asone/demo_ocr.py
+++ asone/demo_ocr.py
@@ -1,19 +1,37 @@
 import asone
 from asone import ASOne
+from .utils import draw_boxes, draw_text
+import cv2
+import argparse
+import time
+import os
 
+def main(args):
+    image_path = args.image
+ 
+    detector = ASOne(asone.CRAFT, recognizer=asone.STANDARD, use_cuda=args.use_cuda)
 
-# Instantiate Asone object
-dt_obj = ASOne(tracker=asone.BYTETRACK, detector=asone.YOLOV7_PYTORCH, use_cuda=True) #set use_cuda=False to use cpu
+    img = cv2.imread(image_path)        
+    results = detector.detect_text(img)
+    img = draw_text(img, results)
+    
+    if args.display:
+        cv2.imshow('Window', img)
 
-filter_classes = ['person'] # set to None to track all classes
+    if args.save:
+        cv2.imwrite("results.jpeg", img)
 
-# ##############################################
-#           To track using video file
-# ##############################################
-# Get tracking function
-track_fn = dt_obj.track_video('data/sample_videos/test.mp4', output_dir='data/', save_result=True, display=False, filter_classes=filter_classes)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        exit()
+    
 
-# Loop over track_fn to retrieve outputs of each frame 
-for bbox_details, frame_details in track_fn:
-    bbox_xyxy, ids, scores, class_ids = bbox_details
-    frame, frame_num, fps = frame_details
+if __name__=='__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="Path of image")
+    parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda', help='If provided the model will run on cpu otherwise it will run on gpu')
+    parser.add_argument('--no_display', action='store_false', default=True, dest='display', help='if provided video will not be displayed')
+    parser.add_argument('--no_save', action='store_false', default=True, dest='save', help='if provided video will not be saved')
+
+    args = parser.parse_args()
+    main(args)
