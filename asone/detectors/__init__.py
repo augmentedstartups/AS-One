c633320753e92aa42343dc217b94187b3500cc76
iamMeharRamzan <muhammad.ramzan@axcelerate.ai>
2023-03-13 15:53:32 +0500

    Add Text detector

--- asone/detectors/__init__.py
+++ asone/detectors/__init__.py
@@ -3,6 +3,7 @@ from asone.detectors.yolov6 import YOLOv6Detector
 from asone.detectors.yolov7 import YOLOv7Detector
 from asone.detectors.yolor import YOLOrDetector
 from asone.detectors.yolox import YOLOxDetector
+from asone.detectors.easyocr_detector import TextDetector
 
 from asone.detectors.detector import Detector
 __all__ = ['Detector'
@@ -10,4 +11,5 @@ __all__ = ['Detector'
            'YOLOv6Detector',
            'YOLOv7Detector',
            'YOLOrDetector',
-           'YOLOxDetector']
+           'YOLOxDetector',
+           'TextDetector']
