21916c6218ade4c1b96e6d5959b1e8cc6f95648b
Devax <devax@Shehryars-MacBook-Air.local>
2023-04-10 17:10:12 +0500

    Copy all paths in json

--- asone/utils/download.py
+++ asone/utils/download.py
@@ -1,6 +1,7 @@
 import gdown
 import os
 import zipfile
+import json
 
 
 def exractfile(file, dest):
@@ -9,111 +10,15 @@ def exractfile(file, dest):
 
 
 def download_weights(weights):
-
+    
+    f = open('asone/utils/weights.json')
+    data = json.load(f)
+    
     outputpath = os.path.dirname(weights)
     model = os.path.splitext(os.path.basename(weights))[0]
     filename = f'{model}.zip'
-    if model == 'yolov5s':
-        # model_key = '1H7G8ryDXs6bKlK2Qot7-2uIkjEYoYook'
-        model_key = '1A1IjE8UBov8GOY8SE0-cCGcU3oy6VnkA'
-    elif model == 'yolov5x6':
-        # model_key = '161bThpOB4HDqrh2FXvbFZJmiSKFwS_Wb'
-        model_key = '1HbcpT9WphK8f1F06YaO-zV7RjW0yDbuI'
-    elif model == 'yolov5n':
-        # model_key = '1zI4f0AUHAz-fTE_fP7UyiFSRGBYYXd7x'
-        model_key = '1oBIxpRbtOnSIJ95P8eYi4PRV4RecOmlY'
-    elif model == 'yolov5m':
-        # model_key = '1vy8S68wbUzKSHMhsTuLN-VA7lMzKchAa'
-        model_key = '1cackvhul4PcKPWrfIwWCqUtONxn95zNw'
-    elif model == 'yolov5l':
-        # model_key = '1pQL9s0o3v6CycAgAX8SkxCfordUl5IxZ'
-        model_key = '1MWxst9otehvQqf3PV2HsaQxnxWsNGlwi'
-    elif model == 'yolov5x':
-        # model_key = '1iB7MQ1IP3MVKLMF8TIJ44vtv9cjWC2qH'
-        model_key = '1dXb0JsqPBPXil97AQOp2ayABKB66hF6E'
-    elif model == 'yolov5n6':
-        # model_key = '1YxnRYlPcCqXGbX20kPlfSimNfROKwoJH'
-        model_key = '15_z39PSn5TWqf1BwLPsSZaWmZHubsKUd'
-    elif model == 'yolov5s6':
-        # model_key = '1mm5zY6IpPtM7IZh_X5x0kAxuO7INKyte'
-        model_key = '1lPw70jiMU6NAiz6E-Vazjsk628QjdjYB'
-    elif model == 'yolov5m6':
-        # model_key = '1qv_uan5oNq9skcg1UThfaFs0xMs2mSE2'
-        model_key = '1K3N06o6fs9LQKcrsdW99-ShbeXoPj4cD'
-    elif model == 'yolov5l6':
-        # model_key = '1eaM51cIh8i_EXmg6Nf0Sx2uW53pT7wZR'
-        model_key = '1B__MWodCdijf1zzYQekuUxFY_laVl6IQ'
-    elif model == 'yolov6n':
-        model_key = '1NA_u4BkPE_N8HcPmZrd7HyLmvFHOk8qd'
-    elif model == 'yolov6t':
-        model_key = '16OWncBp-vh-sLDMOR58th3WOGv4envQ1'
-    elif model == 'yolov6s':
-        model_key = '14BE0j654ClLxMq2ySWZNhTCmf48mLyXi'
-    elif model == 'yolov6l_relu':
-        model_key = '14UfY057QUQoAj6q39PX_qE7U1bBIrIGi'
-    elif model == 'yolov6l':
-        model_key = '1HdRIs0uMPbqs5E2aEX8O3d3dJTh-KBTf'
-    elif model == 'yolov6m':
-        model_key = '1t_w9SCwbZAW7icwX_z97-SQz-plXzBgM'
-    elif model == 'yolov6s_repopt':
-        model_key = '1L_1Crxx-4059xDDUZEf_asWRBVd3PF05'
-    elif model == 'yolov7-e6e':
-        model_key = '1rQR5KiSJiWtpHEniAyeBQdpXFb7Wv1UT'
-    elif model == 'yolov7-d6':
-        model_key = '1idAyjdq9pVsgkDCCfADbGOjxGq4TPulB'
-    elif model == 'yolov7':
-        model_key = '10XNOpBAmMrYqmXOsJLl79MGtuGWY2zAl'
-    elif model == 'yolov7-tiny':
-        model_key = '1ut2doFvtQSKGjiHGPBsEItZlTTj-7_rF'
-    elif model == 'yolov7-e6':
-        model_key = '1E9pow2PFcvil0iqRx2tRCI4HLduh9gp0'
-    elif model == 'yolov7-w6':
-        model_key = '1B8j9XMZxGxz8kpsqJhKXuk1TE_244n6t'
-    elif model == 'yolov7x':
-        model_key = '1FiGLXG6_3He21ean4bFET471Wrj-3oc3'
-    elif model == 'yolor_csp':
-        model_key = '1G3FBZKrznW_64mGfs6b3nAJiJv6GmmV0'
-    elif model == 'yolor_csp_star':
-        model_key = '15WDl46ZthFGZfpOyI3qXx6gC9FQLH_wH'
-    elif model == 'yolor_csp_x':
-        model_key = '1LU2ckh7eSpVD0nyPSdq1n34lKmNAX39T'
-    elif model == 'yolor_csp_x_star':
-        model_key = '1jheqFDm7BpHQpR60wuWSBpbuyK5SoKdV'
-    elif model == 'yolor_p6':
-        model_key = '1XKREKdxQCO8OXiW2IWGFhczolIaIr9sm'
-    elif model == 'yolox_l':
-        model_key = '1jX1KHerOdZ5-dmXh6cWcRAn80aKD-7sP'
-    elif model == 'yolox_nano':
-        model_key = '1783Os6uTpYjunL-MfK0WE1Wcwk58fIUi'
-    elif model == 'yolox_tiny':
-        model_key = '1Lcv1ITvfPdWsu6Kb8Hq6cOSfJE7lbbf2'
-    elif model == 'yolox_darknet':
-        model_key = '17f4UI06TWJ25Oqo2OoQGu8AoGVX1lPta'
-    elif model == 'yolox_s':
-        model_key = '1IUAOv62XuwkwwTCVD2y3xJg7KUA3M0-M'
-    elif model == 'yolox_m':
-        model_key = '1ktHj8UEwl0V8Qz9G74E-yj-o13FLeD0-'
-    elif model == 'yolox_x':
-        model_key = '13HNnlILCx_XamNJWwJ1MG5x0XfP6HL1U'
-    elif model == 'ckpt':
-        model_key = '1VZ05gzg249Q1m8BJVQxl3iHoNIbjzJf8'
-
-    elif model == 'yolov8s':
-        # model_key = '1rokjGeiLlLSNugd6LuGQj6Yr_i5_XH3Y'
-        model_key = '1hUhjQWw1cJL7TtBG0zD3MO52iCYh0DEn'
-    elif model == 'yolov8n':
-        # model_key = '1JslnzKzY7bHRQWiLfvcINteIOgyOv_oU'
-        model_key = '1x6zHzsEcyhuyWy2xY3swAQ4vIQvBYrsr'
-    elif model == 'yolov8l':
-        # model_key = '1Zlp3e9gBQtgt76SHWpRNdEw4rXeT4GxE'
-         model_key = '1xQxHTEIpoiP4d73F6dtedU7hIZpFTqY2'
-    elif model == 'yolov8m':
-        # model_key = '1ijE_fou-U-UJb4xRspFK0OnPfsLcuw3U'
-        model_key = '1_FoKnqkaoWchVy4B24Hn2PanEKfh-eSp'
-    elif model == 'yolov8x':
-        # model_key = '1vtkXtgSLG49l-mh8zzgF9xZdM1ZuRldI'
-        model_key = '1s60fsjiyDlPQ1L5H_GAoWahHONLbvz7T'
-        
+    if model in data:
+        model_key = data[model]
     else:
         raise ValueError(f'No model named {model} found.')
 
@@ -125,3 +30,106 @@ def download_weights(weights):
 
     exractfile(filename, outputpath)
     os.remove(filename)
+
+# ..........................................................................................
+# if model == 'yolov5s':
+#         # model_key = '1H7G8ryDXs6bKlK2Qot7-2uIkjEYoYook'
+#         model_key = '1A1IjE8UBov8GOY8SE0-cCGcU3oy6VnkA'
+#     elif model == 'yolov5x6':
+#         # model_key = '161bThpOB4HDqrh2FXvbFZJmiSKFwS_Wb'
+#         model_key = '1HbcpT9WphK8f1F06YaO-zV7RjW0yDbuI'
+#     elif model == 'yolov5n':
+#         # model_key = '1zI4f0AUHAz-fTE_fP7UyiFSRGBYYXd7x'
+#         model_key = '1oBIxpRbtOnSIJ95P8eYi4PRV4RecOmlY'
+#     elif model == 'yolov5m':
+#         # model_key = '1vy8S68wbUzKSHMhsTuLN-VA7lMzKchAa'
+#         model_key = '1cackvhul4PcKPWrfIwWCqUtONxn95zNw'
+#     elif model == 'yolov5l':
+#         # model_key = '1pQL9s0o3v6CycAgAX8SkxCfordUl5IxZ'
+#         model_key = '1MWxst9otehvQqf3PV2HsaQxnxWsNGlwi'
+#     elif model == 'yolov5x':
+#         # model_key = '1iB7MQ1IP3MVKLMF8TIJ44vtv9cjWC2qH'
+#         model_key = '1dXb0JsqPBPXil97AQOp2ayABKB66hF6E'
+#     elif model == 'yolov5n6':
+#         # model_key = '1YxnRYlPcCqXGbX20kPlfSimNfROKwoJH'
+#         model_key = '15_z39PSn5TWqf1BwLPsSZaWmZHubsKUd'
+#     elif model == 'yolov5s6':
+#         # model_key = '1mm5zY6IpPtM7IZh_X5x0kAxuO7INKyte'
+#         model_key = '1lPw70jiMU6NAiz6E-Vazjsk628QjdjYB'
+#     elif model == 'yolov5m6':
+#         # model_key = '1qv_uan5oNq9skcg1UThfaFs0xMs2mSE2'
+#         model_key = '1K3N06o6fs9LQKcrsdW99-ShbeXoPj4cD'
+#     elif model == 'yolov5l6':
+#         # model_key = '1eaM51cIh8i_EXmg6Nf0Sx2uW53pT7wZR'
+#         model_key = '1B__MWodCdijf1zzYQekuUxFY_laVl6IQ'
+#     elif model == 'yolov6n':
+#         model_key = '1NA_u4BkPE_N8HcPmZrd7HyLmvFHOk8qd'
+#     elif model == 'yolov6t':
+#         model_key = '16OWncBp-vh-sLDMOR58th3WOGv4envQ1'
+#     elif model == 'yolov6s':
+#         model_key = '14BE0j654ClLxMq2ySWZNhTCmf48mLyXi'
+#     elif model == 'yolov6l_relu':
+#         model_key = '14UfY057QUQoAj6q39PX_qE7U1bBIrIGi'
+#     elif model == 'yolov6l':
+#         model_key = '1HdRIs0uMPbqs5E2aEX8O3d3dJTh-KBTf'
+#     elif model == 'yolov6m':
+#         model_key = '1t_w9SCwbZAW7icwX_z97-SQz-plXzBgM'
+#     elif model == 'yolov6s_repopt':
+#         model_key = '1L_1Crxx-4059xDDUZEf_asWRBVd3PF05'
+#     elif model == 'yolov7-e6e':
+#         model_key = '1rQR5KiSJiWtpHEniAyeBQdpXFb7Wv1UT'
+#     elif model == 'yolov7-d6':
+#         model_key = '1idAyjdq9pVsgkDCCfADbGOjxGq4TPulB'
+#     elif model == 'yolov7':
+#         model_key = '10XNOpBAmMrYqmXOsJLl79MGtuGWY2zAl'
+#     elif model == 'yolov7-tiny':
+#         model_key = '1ut2doFvtQSKGjiHGPBsEItZlTTj-7_rF'
+#     elif model == 'yolov7-e6':
+#         model_key = '1E9pow2PFcvil0iqRx2tRCI4HLduh9gp0'
+#     elif model == 'yolov7-w6':
+#         model_key = '1B8j9XMZxGxz8kpsqJhKXuk1TE_244n6t'
+#     elif model == 'yolov7x':
+#         model_key = '1FiGLXG6_3He21ean4bFET471Wrj-3oc3'
+#     elif model == 'yolor_csp':
+#         model_key = '1G3FBZKrznW_64mGfs6b3nAJiJv6GmmV0'
+#     elif model == 'yolor_csp_star':
+#         model_key = '15WDl46ZthFGZfpOyI3qXx6gC9FQLH_wH'
+#     elif model == 'yolor_csp_x':
+#         model_key = '1LU2ckh7eSpVD0nyPSdq1n34lKmNAX39T'
+#     elif model == 'yolor_csp_x_star':
+#         model_key = '1jheqFDm7BpHQpR60wuWSBpbuyK5SoKdV'
+#     elif model == 'yolor_p6':
+#         model_key = '1XKREKdxQCO8OXiW2IWGFhczolIaIr9sm'
+#     elif model == 'yolox_l':
+#         model_key = '1jX1KHerOdZ5-dmXh6cWcRAn80aKD-7sP'
+#     elif model == 'yolox_nano':
+#         model_key = '1783Os6uTpYjunL-MfK0WE1Wcwk58fIUi'
+#     elif model == 'yolox_tiny':
+#         model_key = '1Lcv1ITvfPdWsu6Kb8Hq6cOSfJE7lbbf2'
+#     elif model == 'yolox_darknet':
+#         model_key = '17f4UI06TWJ25Oqo2OoQGu8AoGVX1lPta'
+#     elif model == 'yolox_s':
+#         model_key = '1IUAOv62XuwkwwTCVD2y3xJg7KUA3M0-M'
+#     elif model == 'yolox_m':
+#         model_key = '1ktHj8UEwl0V8Qz9G74E-yj-o13FLeD0-'
+#     elif model == 'yolox_x':
+#         model_key = '13HNnlILCx_XamNJWwJ1MG5x0XfP6HL1U'
+#     elif model == 'ckpt':
+#         model_key = '1VZ05gzg249Q1m8BJVQxl3iHoNIbjzJf8'
+
+#     elif model == 'yolov8s':
+#         # model_key = '1rokjGeiLlLSNugd6LuGQj6Yr_i5_XH3Y'
+#         model_key = '1hUhjQWw1cJL7TtBG0zD3MO52iCYh0DEn'
+#     elif model == 'yolov8n':
+#         # model_key = '1JslnzKzY7bHRQWiLfvcINteIOgyOv_oU'
+#         model_key = '1x6zHzsEcyhuyWy2xY3swAQ4vIQvBYrsr'
+#     elif model == 'yolov8l':
+#         # model_key = '1Zlp3e9gBQtgt76SHWpRNdEw4rXeT4GxE'
+#          model_key = '1xQxHTEIpoiP4d73F6dtedU7hIZpFTqY2'
+#     elif model == 'yolov8m':
+#         # model_key = '1ijE_fou-U-UJb4xRspFK0OnPfsLcuw3U'
+#         model_key = '1_FoKnqkaoWchVy4B24Hn2PanEKfh-eSp'
+#     elif model == 'yolov8x':
+#         # model_key = '1vtkXtgSLG49l-mh8zzgF9xZdM1ZuRldI'
+#         model_key = '1s60fsjiyDlPQ1L5H_GAoWahHONLbvz7T'
+        
