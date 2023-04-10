c8439aefd365942ee1a637a86298e40737745ff1
Devax <devax@Shehryars-MacBook-Air.local>
2023-04-10 15:11:24 +0500

    Adding support for coreml -->working

--- asone/detectors/yolov7/yolov7_detector.py
+++ asone/detectors/yolov7/yolov7_detector.py
@@ -2,6 +2,7 @@ import os
 import sys
 import onnxruntime
 import torch
+import coremltools as ct
 from asone.utils import get_names
 import numpy as np
 import warnings
@@ -10,14 +11,28 @@ from asone.detectors.yolov7.yolov7.utils.yolov7_utils import (prepare_input,
                                  non_max_suppression)
 from asone.detectors.yolov7.yolov7.models.experimental import attempt_load
 from asone import utils
+from PIL import Image
+from asone.detectors.utils.coreml_utils import yolo_to_xyxy, generalize_output_format, scale_bboxes
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'yolov7'))
 class YOLOv7Detector:
     def __init__(self,
                  weights=None,
                  use_onnx=False,
+                 mlmodel=False,
                  use_cuda=True):
         self.use_onnx = use_onnx
+        self.mlmodel = mlmodel
         self.device = 'cuda' if use_cuda else 'cpu'
 
         #If incase weighst is a list of paths then select path at first index
@@ -42,6 +57,9 @@ class YOLOv7Detector:
                 providers = ['CPUExecutionProvider']
 
             model = onnxruntime.InferenceSession(weights, providers=providers)
+        # Load coreml
+        elif self.mlmodel:
+            model = ct.models.MLModel(weights)
         #Load Pytorch
         else: 
             model = attempt_load(weights, map_location=self.device)
@@ -69,6 +87,70 @@ class YOLOv7Detector:
             input_name = self.model.get_inputs()[0].name
             prediction = self.model.run([self.model.get_outputs()[0].name], {
                                  input_name: processed_image})
+        # Run Coreml model 
+        elif self.mlmodel:
+            
+            # h ,w = image.shape[:2]
+            # print(pred.keys())
+            # y = self.model.predict({'image': image})  # coordinates are xywh normalized
+            y = self.model.predict({"image":Image.fromarray(image).resize(input_shape)})
+            # print(y)
+            print(y.keys())
+            print(y['var_1019'].shape)
+            print(y['var_1034'].shape)
+            print(y['var_1049'].shape)
+            exit()
+            if 'confidence' in y:
+                box = xywh2xyxy(y['coordinates'] * [[w, h, w, h]])  # xyxy pixels
+                conf, cls = y['confidence'].max(1), y['confidence'].argmax(1).astype(np.float)
+                y = np.concatenate((box, conf.reshape(-1, 1), cls.reshape(-1, 1)), 1)
+            else:
+                k = 'var_' + str(sorted(int(k.replace('var_', '')) for k in y)[-1])  # output key
+                y = y[k]  # output
+                print(y)
+                print(y.shape)
+                exit()
+                        # Reshape the output to a 2D array with 85 columns
+            y = y.reshape((1, -1, 85))
+            prediction = torch.from_numpy(y)
+            # Extract the bounding boxes and convert them to [x1, y1, x2, y2] format
+            # boxes = y[:, :, :4]
+            # boxes = np.concatenate((boxes[:, :, :2] - boxes[:, :, 2:] / 2, boxes[:, :, :2] + boxes[:, :, 2:] / 2), axis=2)
+
+            # # Extract the objectness scores and class probabilities
+            # objectness_scores = y[:, :, 4]
+            # class_probs = y[:, :, 5:]
+
+            # # Compute the maximum class probability for each location
+            # class_scores = np.max(class_probs, axis=2)
+            # class_ids = np.argmax(class_probs, axis=2)
+            # print(boxes)
+            # print(class_scores)
+            # print(len(class_ids[0]))
+            # # print(detection)
+            # exit()
+            # print(y)
+            # print(y.shape)
+            # exit()
+            # xyxy = yolo_to_xyxy(pred['coordinates'], input_shape)
+            # out = generalize_output_format(xyxy, pred['confidence'], conf_thres)
+            # detections = scale_bboxes(out, image.shape[:2], input_shape)
+            
+            # if filter_classes:
+            #     class_names = get_names()
+
+            #     filter_class_idx = []
+            #     if filter_classes:
+            #         for _class in filter_classes:
+            #             if _class.lower() in class_names:
+            #                 filter_class_idx.append(class_names.index(_class.lower()))
+            #             else:
+            #                 warnings.warn(f"class {_class} not found in model classes list.")
+
+            #     detections = detections[np.in1d(detections[:,5].astype(int), filter_class_idx)]
+            
+            # return detections, {'width':w, 'height':h}
+            
         # Run Pytorch model
         else:
             processed_image = torch.from_numpy(processed_image).to(self.device)
@@ -81,6 +163,7 @@ class YOLOv7Detector:
         detection = []
         # Postprocess prediction
         if self.use_onnx:
+            
             detection = process_output(prediction,
                                        original_image.shape[:2],
                                        input_shape,
