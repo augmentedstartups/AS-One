a94f810edaa9f7534d24c2176ba439cc6b6e3d93
iamMeharRamzan <muhammad.ramzan@axcelerate.ai>
2023-04-27 15:12:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-from asone.pose_estimator import PoseEstimator
+import asone
+from asone import PoseEstimator
 
-PoseEstimator()
+video_path = 'data/sample_videos/football1.mp4'
+pose_estimator = PoseEstimator(estimator_flag=asone.YOLOV7_W6_POSE, use_cuda=True)
+# pose_estimator = PoseEstimator(estimator_flag=asone.YOLOV8M_POSE, use_cuda=True)
+pose_estimator.estimate_video(video_path, save=True, display=False)
