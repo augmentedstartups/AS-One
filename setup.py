d1bc3a330697f2e340c673c901489da0099fcea6
iamMeharRamzan <muhammad.ramzan@axcelerate.ai>
2023-06-03 12:15:50 +0500

    Update version 0.3.1 --> 0.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ MAINTAINER_EMAIL = 'umair.imran@axcelerate.ai'
 URL = 'https://github.com/axcelerateai/asone'
 DOWNLOAD_URL = URL
 
-VERSION = '0.3.1'
+VERSION = '0.3.2'
 
 with open('README.md') as f:
     long_description = f.read()
